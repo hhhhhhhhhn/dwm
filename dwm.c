c3ede7f3b933827cd2d162bc795e00b8080a840b
hhhhhhhhhn <joseugarte2262005@gmail.com>
2022-02-02 17:09:45 -0300

    Fix outofstack in focus function

--- dwm.c
+++ dwm.c
@@ -889,7 +889,7 @@ void
 focus(Client *c)
 {
 	if (!c || !ISVISIBLE(c))
-		for (c = selmon->stack; c && !ISVISIBLE(c); c = c->snext);
+		for (c = selmon->stack; c && (!ISVISIBLE(c) || c->outofstack); c = c->snext);
 	if (selmon->sel && selmon->sel != c)
 		unfocus(selmon->sel, 0);
 	if (c) {
